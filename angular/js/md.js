5b6ee947cc156b5e28c648c968ea73c3792eda9f
JiaLe1 <948962131@qq.com>
2017-07-05 00:02:00 +0800

    Add files via upload

---
+++ angular/js/md.js
@@ -0,0 +1,28 @@
+'use strict';
+angular.module('myapp',[])
+	.controller('md',function($scope){
+		$scope.data=localStorage.message?JSON.parse(localStorage.message):[];   //将临时文本保存在本地
+		//添加新笔记
+		var $i=0;
+		$scope.add=function(){
+			$i++;
+			var newnote={};
+			newnote.id=$i;
+			newnote.name="新笔记";
+			newnote.con="在此输入内容...";
+			$scope.data.push(newnote);
+			localStorage.message=angular.toJson($scope.data);
+		}
+		//删除
+		$scope.del=function($index){
+			for(var $i=0;$i<$scope.data.length;$i++){
+				if($index==$i){
+					$scope.data.splice($i,1);
+				}
+			}
+		}
+		//title
+		for(var $i=0;$i<$scope.data.length;$i++){
+			$scope.name=$scope.data[$i].name;
+		}
+	})
